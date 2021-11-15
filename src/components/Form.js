0680798a346d0f2664d48f13c7bdc00a94d0c91f
Jose Tommaso <tommaso_j@hotmail.com>
2021-11-15 23:59:49 +0000

    last commit

--- src/components/Form.js
+++ src/components/Form.js
@@ -11,10 +11,21 @@ const Form = () => {
     const [ website, setWebsite ] = useState("")
     const [ errorMessage, setErrorMessage ] = useState("")
     const [ error, setError ] = useState(false)
-    const [ perPage, setPerPage ] = useState(20)
+    const [ perPage, setPerPage ] = useState(10)
+    const [ selector, setSelector ] = useState('')
     const [ blogJson, setBlogJson ] = useState([])
     const [ isReady, setIsReady ] = useState(false)
     const [ isLoading, setIsLoading ] = useState(false);
+
+    const cheerio = require('cheerio');
+
+    const scrapeBlog = async (url, selector) => {
+        const data = await fetch(`${website}${url}`)
+        const response = await data.text()
+        const $ = cheerio.load(response) 
+        const article = ($(selector).html())
+        return article
+    } 
 
     const validURL = str => {
         var pattern = new RegExp('^(https?:\\/\\/)?'+ // protocol
@@ -25,7 +36,6 @@ const Form = () => {
           '(\\#[-a-z\\d_]*)?$','i'); // fragment locator
         return !!pattern.test(str);
     }
-
 
     const apiCall = async (e) => {
         e.preventDefault()
@@ -58,7 +68,6 @@ const Form = () => {
             const url = `${website}/api/v1/blogs.json?per_page=${perPage}`
             const response = await fetch(url)
             const result = await response.json()
-
             const blogs = result.blogs
 
             let newBlogsObject = []
@@ -66,61 +75,77 @@ const Form = () => {
             blogs.forEach(blog => {
                 blog["tag_list"] = blog["tag_list"].split("|").join(",")
                 blog["publish_date"] = moment(blog["publish_date"]).format("YYYY-MM-DD HH:mm:ss")
+                blog["content"] = `<p class='font-weight-bold'>${blog.content}</p>`
 
-                const { title, content, image, author, publish_date, tag_list } = blog
+                const { title, image, author, publish_date, tag_list, url } = blog
 
-                const newBlogObject = {
-                    "title": title,
-                    "body": content,
-                    "image": image,
-                    "author_name": author,
-                    "author_image": '',
-                    "language": '',
-                    "publish_date": publish_date,
-                    "tags": tag_list
-                }
-
-                newBlogsObject.push(newBlogObject)
+                scrapeBlog(url, selector)
+                    .then((response) => {
+                        const newBlogObject = {
+                            "title": title,
+                            "body": response,
+                            "image": image,
+                            "author_name": author,
+                            "author_image": '',
+                            "language": '',
+                            "publish_date": publish_date,
+                            "tags": tag_list
+                        }
+        
+                        newBlogsObject.push(newBlogObject)
+                        
+                        setBlogJson(newBlogsObject)                   
+        
+                        setError(false)
+                        setIsReady(true)
+                })
                 
-                setBlogJson(newBlogsObject)
-
-                setError(false)
-                setIsReady(true)
             });
-
 
         } catch (error) {
             setError(true)
             setErrorMessage(`${error.message}`)
         }
-        
+
         setIsLoading(false)
         
     }
 
     return (
         <>
-            <div className="card">
+            <div className="card my-5">
                 <div className="card-header">
                     JSON to CSV Blogs downloader
                 </div>
                 <div className="card-body position-relative">
                     <h5 className="card-title">How to use:</h5>
                     <ol className="pl-0">
-                        <li className="mb-2">Introduce a Website URL <span className="d-block">Example: https://www.volcanic.com</span></li>
-                        <li className="mb-2">Type how many blogs you would like in your CSV file. default: 20 blogs</li>
+                        <li className="mb-2">Type a Website URL <span className="d-block">Example: https://www.volcanic.com</span></li>
+                        <li className="mb-2">Type the article selector <span className="d-block">Example: #blog .container .col-md-6 article</span></li>
+                        <li className="mb-2">Type how many blogs you would like in your CSV file. default: 10 blogs</li>
                         <li className="mb-2">Click 'Load JSON' button</li>
                         <li className="mb-2">Relax</li>
                         <li className="mb-2">Download your CSV file</li>
                     </ol>
                     <form>
                         <div className="mb-3">
-                                <label htmlFor="website" type="url" className="form-label">Website URL:</label>
+                                <label htmlFor="website" className="form-label">Website URL:</label>
+                                <input
+                                    className="form-control"
+                                    type="url"
+                                    id="website"
+                                    placeholder="https://www.volcanic.com"
+                                    onChange={e => setWebsite(e.target.value)}
+                                />
+                        </div>
+                        <div className="mb-3">
+                                <label htmlFor="selector" className="form-label">Article selector:</label>
                                 <input
                                     className="form-control"
                                     type="text"
-                                    id="website"
-                                    onChange={e => setWebsite(e.target.value)}
+                                    id="selector"
+                                    placeholder="#blog .container .col-md-6 article"
+                                    onChange={e => setSelector(e.target.value)}
                                 />
                         </div>
                         <div className="mb-3">
